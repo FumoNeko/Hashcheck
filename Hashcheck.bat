f3a9836c6c5086c187334f52df634f58bb0379d0
FumoNeko <28814987+Blockhead5@users.noreply.github.com>
2020-07-10 02:12:48 -0700

    Update Hashcheck.bat
    added linebreaks to make output easier to read

--- Hashcheck.bat
+++ Hashcheck.bat
@@ -1,10 +1,17 @@
 @echo off
 set /p filename="Enter Filepath: "
+@echo(
 CertUtil -hashfile %filename% MD2
+@echo(
 CertUtil -hashfile %filename% MD4
+@echo(
 CertUtil -hashfile %filename% MD5
+@echo(
 CertUtil -hashfile %filename% SHA1
+@echo(
 CertUtil -hashfile %filename% SHA256
+@echo(
 CertUtil -hashfile %filename% SHA384
+@echo(
 CertUtil -hashfile %filename% SHA512
 pause
